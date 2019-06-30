645b13d95b2d877a3bf2648cf7a0260f138c5d0f
Puleeno Nguyen <puleeno@gmail.com>
2019-06-30 22:41:34 +0700

    try to move action to use hook

--- plugins/core/download.php
+++ plugins/core/download.php
@@ -2,18 +2,31 @@
 use Puleeno\Goader\Hook;
 use Puleeno\Goader\Clients\Downloader;
 
-function goader_core_register_download_command($runner, $args, $command)
-{
-    if (empty($args)) {
-        return $runner;
-    }
-    $maybeUrl = end($args);
-    $command = array_shift($args);
+Hook::add_action('goader_init', function () {
+    function goader_core_register_download_command($runner, $args, $command)
+    {
+        if (empty($args)) {
+            return $runner;
+        }
+        $maybeUrl = end($args);
+        $command = array_shift($args);
 
-    if ($command === 'download' || preg_match('/^https?:\/\//', $maybeUrl)) {
-        $downloader = new Downloader($maybeUrl);
+        if ($command === 'download' || preg_match('/^https?:\/\//', $maybeUrl)) {
+            $downloader = new Downloader($maybeUrl);
 
-        return array($downloader, 'run');
+            return array($downloader, 'run');
+        }
     }
-}
-Hook::add_filter('register_goader_command', 'goader_core_register_download_command', 10, 3);
+    Hook::add_filter('register_goader_command', 'goader_core_register_download_command', 10, 3);
+
+    Hook::add_filter('image_sequence_file_name', 'goader_sequence_download_file_name', 10, 3);
+
+    function goader_sequence_download_file_name($fileName, $currentIndex, $data)
+    {
+        $newFileName = $currentIndex;
+        if (!empty($data['file_name_prefix'])) {
+            $newFileName = sprintf('%s-%s', $data['file_name_prefix'], $currentIndex);
+        }
+        return $newFileName;
+    }
+}, 20);
--- plugins/hosts/json.php
+++ plugins/hosts/json.php
@@ -1,43 +1,45 @@
 <?php
 use Puleeno\Goader\Environment;
 use Puleeno\Goader\Hook;
-use Puleeno\Goader\Host\io\Json;
+use Puleeno\Goader\Hosts\io\Json;
 
-Hook::add_filter('custom_none_host', 'detect_json_file_download', 10, 2);
-function detect_json_file_download($host, $command)
-{
-    $jsonFile = $command[0];
-    if (!file_exists($jsonFile)) {
-        return $host;
-    }
-    $jsonStr = file_get_contents($jsonFile);
-    $json = json_decode($jsonStr, true);
+Hook::add_action('goader_init', function () {
+    Hook::add_filter('custom_none_host', 'detect_json_file_download', 10, 2);
+    function detect_json_file_download($host, $command)
+    {
+        $jsonFile = $command[0];
+        if (!file_exists($jsonFile)) {
+            return $host;
+        }
+        $jsonStr = file_get_contents($jsonFile);
+        $json = json_decode($jsonStr, true);
 
-    Hook::add_action('setup_goader_environment', function ($goader) use ($json) {
-        $goader->data['json'] = $json;
-    });
-
-    if ($json) {
-        return Json::class;
-    }
-}
-
-Hook::add_filter('goaders', function ($hosts) {
-    return array_merge($hosts, array(
-        'json' => Json::class
-    ));
-});
-
-Hook::add_action('goader_setup_command', function ($command) {
-    $command->option('h')
-        ->aka('host')
-        ->describedAs('Integrate with host configs via option')
-        ->must(function ($supportedHost) {
-            $supportedHosts = array_keys(Environment::supportedHosters());
-            return in_array($supportedHost, $supportedHosts);
+        Hook::add_action('setup_goader_environment', function ($goader) use ($json) {
+            $goader->data['json'] = $json;
         });
 
-    $command->option('u')
-        ->aka('url')
-        ->describedAs('Url prefix');
-});
+        if ($json) {
+            return Json::class;
+        }
+    }
+
+    Hook::add_filter('goaders', function ($hosts) {
+        return array_merge($hosts, array(
+            'json' => Json::class
+        ));
+    });
+
+    Hook::add_action('goader_setup_command', function ($command) {
+        $command->option('h')
+            ->aka('host')
+            ->describedAs('Integrate with host configs via option')
+            ->must(function ($supportedHost) {
+                $supportedHosts = array_keys(Environment::supportedHosters());
+                return in_array($supportedHost, $supportedHosts);
+            });
+
+        $command->option('u')
+            ->aka('url')
+            ->describedAs('Url prefix');
+    });
+}, 10);
