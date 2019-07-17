0ef840ac555d48e77eec7e61f8183ef2d7644898
Puleeno Nguyen <puleeno@gmail.com>
2019-07-17 18:01:27 +0700

    prepare new features for goader

--- src/Clients/Config.php
+++ src/Clients/Config.php
@@ -4,4 +4,26 @@ namespace Puleeno\Goader\Clients;
 
 class Config
 {
+    protected $supported = ['account', 'host', 'core'];
+
+    public function __construct()
+    {
+    }
+
+    public function account()
+    {
+    }
+
+    /**
+     * List host configs
+     *
+     * - load-cookies: Convert cookies.txt from browser export to CookieJar
+     */
+    public function host()
+    {
+    }
+
+    public function core()
+    {
+    }
 }
