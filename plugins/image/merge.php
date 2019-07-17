1acfcf43ce6631fab81cb8344f7e83ecee32bf03
Puleeno Nguyen <puleeno@gmail.com>
2019-07-18 02:01:09 +0700

    first commit merge tool

--- plugins/image/merge.php
+++ plugins/image/merge.php
@@ -1,2 +1,33 @@
 <?php
+use Puleeno\Goader\Hook;
+use Puleeno\Goader\Clients\Image\Merge;
 
+
+Hook::add_action('goader_init', function () {
+    function goader_core_register_image_merge_command($runner, $args, $command)
+    {
+        if (empty($args)) {
+            return $runner;
+        }
+
+        $command = array_shift($args);
+        if ($command === 'merge') {
+            Hook::add_action('goader_setup_command', 'register_default_image_merge_command_options');
+
+            $mergeClient = new Merge();
+            return array($mergeClient, 'run');
+        }
+    }
+
+    function register_default_image_merge_command_options($command)
+    {
+        $command->option('offset')
+            ->describedAs('Offset chapter');
+
+        $command->option('p')
+            ->aka('prefix')
+            ->describedAs('Use prefix file name');
+    }
+
+    Hook::add_filter('register_goader_command', 'goader_core_register_image_merge_command', 10, 3);
+}, 15);
---
+++ src/Clients/Image/Merge.php
@@ -0,0 +1,14 @@
+<?php
+namespace Puleeno\Goader\Clients\Image;
+
+class Merge
+{
+    public function __construct()
+    {
+    }
+
+    public function run()
+    {
+        die('zo');
+    }
+}
