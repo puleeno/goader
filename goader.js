2bbfdb93a28e0310fc95e70efc44b213c7303d37
Puleeno Nguyen <puleeno@gmail.com>
2019-08-27 00:53:40 +0700

    Update goader.js features

--- goader.js
+++ goader.js
@@ -1,5 +1,7 @@
-const program = require('commander');
 const cloudscraper = require('cloudscraper');
+const FileCookieStore = require('tough-cookie-filestore');
+const program = require('commander');
+const request = require('request');
 const goader = require('./package.json');
 
 program
@@ -8,9 +10,15 @@ program
     .command('*')
     .option('-m', '--method', 'Request method')
     .action(function(env){
-        const options = {};
-        options.uri = env;
-        cloudscraper.get(options).then(console.log).catch(console.error);
+        const j = request.jar(new FileCookieStore('cookiejar.json'));
+        const options = {
+            jar: j,
+            uri: env,
+            method: 'GET',
+            gzip: true,
+        };
+        options.formData = {};
+        cloudscraper(options).then(console.log).catch(console.error);
     });
 
 program.parse(process.argv);
