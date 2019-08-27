a180d28f52177b3db015b1c354f19394bf2cc6a0
Puleeno Nguyen <puleeno@gmail.com>
2019-08-28 02:39:49 +0700

    Improve goader.js

--- goader.js
+++ goader.js
@@ -1,24 +1,49 @@
 const cloudscraper = require('cloudscraper');
 const FileCookieStore = require('tough-cookie-filestore');
-const program = require('commander');
 const request = require('request');
 const goader = require('./package.json');
 
-program
-    .version(goader.version)
-    .description(goader.description)
-    .command('*')
-    .option('-m', '--method', 'Request method')
-    .action(function(env){
-        const j = request.jar(new FileCookieStore('cookiejar.json'));
-        const options = {
-            jar: j,
-            uri: env,
-            method: 'GET',
-            gzip: true,
-        };
-        options.formData = {};
-        cloudscraper(options).then(console.log).catch(console.error);
-    });
 
-program.parse(process.argv);
+require('yargs')
+  .scriptName("goader")
+  .usage('$0 <cmd> [args]')
+  .command('request [method] [cookies] [form-data] url', 'Make the HTTP request with cloudscraper', (yargs) => {
+    yargs.positional('method', {
+      type: 'string',
+      default: 'GET',
+      describe: 'The HTTP request method'
+    });
+    yargs.positional('form-data', {
+        type: 'string',
+        default: '',
+        describe: 'The form data use to submit to the request'
+    });
+    yargs.positional('headers', {
+      type: 'string',
+      default: '',
+      describe: 'The request header'
+  });
+    yargs.positional('cookies', {
+        type: 'string',
+        default: '',
+        describe: 'Cookiejar file path'
+    });
+  }, function (argv) {
+    const options = {
+        uri: argv.url,
+        method: argv.method,
+        gzip: true,
+    };
+    if (argv.formData) {
+        options.formData = JSON.parse(argv.formData);
+    }
+    if (argv.cookies) {
+        options.jar = request.jar(new FileCookieStore(argv.cookies));
+    }
+    if (argv.headers) {
+      options.headers = JSON.parse(argv.headers);
+    }
+    cloudscraper(options).then(console.log).catch(console.error);
+  })
+  .help()
+  .argv
