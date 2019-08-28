1196b3ee5277dadc390048f525b3520c78e403c9
Puleeno Nguyen <puleeno@gmail.com>
2019-08-29 02:35:04 +0700

    Added host iqiyi

---
+++ src/Hosts/com/Iqiyi.php
@@ -0,0 +1,148 @@
+<?php
+namespace Puleeno\Goader\Hosts\com;
+
+use GuzzleHttp\Client;
+use Puleeno\Goader\Abstracts\Host;
+use Puleeno\Goader\Command;
+use Puleeno\Goader\Environment;
+use Puleeno\Goader\Hook;
+use Puleeno\Goader\Logger;
+
+class Iqiyi extends Host
+{
+    const NAME = 'iqiyi';
+
+    protected $dom;
+
+    protected function checkPageType()
+    {
+        /**
+         * 1: Manga
+         * 2: Chapter
+         */
+        $pat = '/manhua\/(reader|detail)[\/|_].+$/';
+        if (preg_match($pat, $this->url, $matches)) {
+            if ($matches[1] === 'detail') {
+                return 1;
+            }
+            return 2;
+        }
+        return false;
+    }
+
+    public function getExtension($extension)
+    {
+        return 'jpg';
+    }
+
+    public function download($directoryName = null)
+    {
+        if (!empty($directoryName)) {
+            $this->dirPrefix = $directoryName;
+        }
+        $page = $this->checkPageType();
+        if ($page === false) {
+            $this->doNotSupport();
+            return;
+        }
+        if ($page === 1) {
+            $this->downloadManga();
+        } else {
+            $this->downloadChapter();
+        }
+    }
+
+    protected function makeChapterNum($text)
+    {
+        if (preg_match('/(\d{1,})/', $text, $matches)) {
+            return sprintf('Chap %s', $matches[1]);
+        }
+        return $text;
+    }
+
+    public function downloadManga()
+    {
+        $this->content = (string)$this->getContent();
+        $this->dom->load($this->content);
+
+        $domChapters = $this->dom->find('#cata_cont_list ol li a');
+        $chapters = array();
+        foreach ($domChapters as $chapter) {
+            $chapters[] = array(
+                'chapter_link' => sprintf('%s://%s%s', $this->host['scheme'], $this->host['host'], $chapter->getAttribute('href')),
+                'chapter_text' => $this->makeChapterNum($chapter->find('span.itemcata-title')->text)
+            );
+        }
+
+        $chapters = array_reverse($chapters);
+
+        Logger::log(sprintf('This manga has %d chapters', count($chapters)));
+        Logger::log('Downloading...');
+
+        foreach ($chapters as $chapter) {
+            $chapter_downloader = new self($chapter['chapter_link'], $this->host);
+            $chapter_downloader->download($chapter['chapter_text']);
+        }
+        Logger::log('The manga is downloaded successfully!!');
+    }
+
+    public function getImagesFromHTML($content)
+    {
+        $images = [];
+        $this->dom->load($content);
+        $dom_images = $this->dom->find('ul.main-container li.main-item img');
+        foreach($dom_images as $dom_image) {
+            $images[] = $dom_image->getAttribute('data-original');
+        }
+        return $images;
+    }
+
+
+    public function downloadChapter()
+    {
+        $this->content = (string)$this->getContent();
+        $images = $this->getImagesFromHTML($this->content);
+        $total_images = count($images);
+        if ($this->dirPrefix) {
+            Logger::log(sprintf('The %s has %s images', strtolower($this->dirPrefix), $total_images));
+        } else {
+            Logger::log(sprintf('This chapter has %s images', $total_images));
+        }
+
+        if ($total_images > 0) {
+            $httpClient = new Client();
+            foreach ($images as $index => $image) {
+                Environment::setCurrentIndex($index + 1);
+                try {
+                    Logger::log(sprintf('Downloading image #%d has URL %s.', $index + 1, $image));
+                    $image_url = $this->formatLink($image);
+                    if (!$this->validateLink($image_url)) {
+                        Logger::log(sprintf('The url #%d is invalid with value "%s"', $index + 1, $image_url));
+                        continue;
+                    }
+
+                    $fileName = $this->generateFileName($image_url, false);
+                    $this->getContent($image_url, $httpClient)->saveFile($fileName);
+                } catch (\Exception $e) {
+                    Logger::log($e->getMessage());
+                }
+            }
+            if ($this->dirPrefix) {
+                Logger::log(sprintf('The %s is downloaded successfully!!', strtolower($this->dirPrefix)));
+            } else {
+                Logger::log(sprintf('The chapter is downloaded successfully!!'));
+            }
+            unset($images);
+        }
+    }
+
+    public function formatLink($originalUrl)
+    {
+        $link = $originalUrl;
+        $pre = Hook::apply_filters('u17_filter_image_link', false, $link);
+        if ($pre) {
+            return $pre;
+        }
+        return $link;
+    }
+}
