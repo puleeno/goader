a84ab3ac3d4e1ef1217ebf640397d576d5b66c4a
Puleeno Nguyen <puleeno@gmail.com>
2019-09-17 00:49:54 +0700

    Added info to download bookcube

---
+++ src/Hosts/com/Bookcube.php
@@ -0,0 +1,11 @@
+<?php
+namespace Puleeno\Goader\Hosts\com;
+
+class Bookcube {
+    // https://toon.bookcube.com/toon/viewer/api.asp
+    // webtoon_num: 190105
+    // split_num: 001
+    // buytype: buy
+    // Referer: https://toon.bookcube.com/toon/viewer/viewer.asp?webtoon_num=190105&split_num=001&buytype=buy
+    // Request Method: POST
+}
